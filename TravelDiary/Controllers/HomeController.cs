e9d530a37537152de57c27de3df8195385e638c0
Antonio Cruz, Dana Luukko and Jamison Cozart <jamison@>
2020-03-11 11:52:36 -0700

    Add all routes and update methods

--- TravelDiary/Controllers/HomeController.cs
+++ TravelDiary/Controllers/HomeController.cs
@@ -1,12 +1,48 @@
-
+using System;
 using Microsoft.AspNetCore.Mvc;
 using TravelDiary.Models;
-
+using System.Collections.Generic;
 namespace TravelDiary.Controllers
 {
     public class HomeController : Controller
     {
+      public int idToEdit = 1;
+
       [HttpGet("/")]
       public ActionResult Home() { return View(); }
+
+      [HttpGet("place/new")]
+      public ActionResult New() { return View(); }
+
+      [HttpGet("/place")] 
+      public ActionResult Index() 
+      {
+      List<Place> newPlace = Place.GetPlace();
+      return View(newPlace);
+      }
+      
+      [HttpPost("/place")] 
+      public ActionResult Create(string description, string name, string date)
+      { 
+        Place newPlace = new Place(description, name, date);
+        return RedirectToAction("Index"); 
+      }
+
+      [HttpPost("/place/{id}")]
+      public ActionResult Update(string description, string name, string date)
+      {
+        Place.Find(idToEdit).Description = description;
+        Place.Find(idToEdit).Name = name;
+        Place.Find(idToEdit).Date = date;
+        return RedirectToAction("Index");
+      }
+
+      [HttpGet("/place/{id}/edit")]
+      public ActionResult Edit(int id)
+      {
+        idToEdit = id;
+        Place placeToEdit = Place.Find(id);
+        return View(placeToEdit);
+      }
     }
 }
--- TravelDiary/Models/Place.cs
+++ TravelDiary/Models/Place.cs
@@ -1,11 +1,33 @@
+using System.Collections.Generic;
+using System;
 namespace TravelDiary.Models
 {
   public class Place
   {
     public string Description { get; set; }
-    public Place(string description)
+    public string Name { get; set; }
+    public string Date { get; set; } 
+
+    public int Id { get; }
+
+    private static List<Place> _newPlace = new List<Place> { }; 
+    public Place(string description, string name, string date)
     {
       Description = description;
+      Name = name;
+      Date = date;
+      _newPlace.Add(this);
+      Id= _newPlace.Count;
+    }
+
+    public static List<Place> GetPlace()
+    {
+      return _newPlace;
+    }
+
+    public static Place Find(int searchId)
+    {
+      return _newPlace[searchId-1];
     }
   }
 }
